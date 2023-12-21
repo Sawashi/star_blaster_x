2623fd89efad1e2b751443a1d3a6dda0cbb6b987
Quốc Bảo <81245741+Pheodrys-QB@users.noreply.github.com>
2023-12-21 13:47:16 +0700

    Fix conflict

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class Enemy : MonoBehaviour {
-    public int health = 2;
+    public float health = 2;
     public float speed = 0.5f;
     public abstract void Hit();
     public abstract void Die();
