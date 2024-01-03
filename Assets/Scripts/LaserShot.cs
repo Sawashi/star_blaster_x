89c417c6d81a1080e1f81b983af7a6fe6c2a773f
Kiet <92447748+Sawashi@users.noreply.github.com>
2024-01-03 20:05:50 +0700

    Remake level 2

--- Assets/Scripts/LaserShot.cs
+++ Assets/Scripts/LaserShot.cs
@@ -3,20 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class LaserShot : Bullet {
-
-
-
-
-    private void OnTriggerStay2D(Collider2D collision) {
-        if (collision.gameObject.tag == "Enemy") {
+public class LaserShot : Bullet
+{
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
             Enemy enemy = collision.transform.parent.GetComponent<Enemy>();
-            if (enemy != null) {
+            if (enemy != null)
+            {
                 enemy.Hit(damage);
 
             }
-
-
+        }
+        if (collision.gameObject.tag == "NewEnemy")
+        {
+            rigidbody.velocity = Vector2.zero;
+            animator.SetBool("Hit", true);
+            collision.gameObject.GetComponent<EnemyController>().takeEnemyHealth(5, "Laser");
+            collider.enabled = false;
         }
     }
 
